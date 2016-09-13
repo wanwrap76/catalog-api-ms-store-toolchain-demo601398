900e60f6005fa4dce1dcc3ccba89881b373fbd17
Andrew J. Lohr <ajlohr@us.ibm.com>
2016-09-13 17:08:43 -0400

    register app again if sd returns 410

--- service-discovery/service-discovery.js
+++ service-discovery/service-discovery.js
@@ -11,6 +11,7 @@ serviceDiscoveryURL = serviceDiscoveryCreds.credentials.url;
 
 // Register this application with Service Discovery.
 exports.register = function(){
+	console.log("called register");
 	var requestOptions = {
 		url: serviceDiscoveryURL + "/api/v1/instances",
 		method: "POST",
@@ -42,7 +43,7 @@ exports.register = function(){
 // Service Discovery requires you to send a PUT request to a heartbeat url every so often so the discovery service
 // knows this application is still healthy. We will send a heartbeat when we are halfway through the ttl (time to live).
 function setHeartbeat(heartbeatURL, ttl){
-	setInterval(function () {
+	var heartBeatInterval = setInterval(function () {
 		var heartbeatOptions = {
 			url:heartbeatURL, 
 			method: "PUT",
@@ -51,7 +52,12 @@ function setHeartbeat(heartbeatURL, ttl){
 			}
 		}
 		request(heartbeatOptions, function (err, response, body) {
-			if (err || response.statusCode != 200) {
+			if(response.statusCode == 410){
+				console.log("Catalog application was no longer registered with service discovery. Reregistering application...");
+				clearInterval(heartBeatInterval);
+				exports.register();
+			}
+			else if (err || response.statusCode != 200) {
 				console.log("Heartbeat request failed. " + response.body);
 			}
 		});
